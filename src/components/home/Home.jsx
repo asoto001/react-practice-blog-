20aa71de9166d8316463d6967b90164efa556348
alejandro <soto.millan.ceni@gmail.com>
2023-02-19 10:55:02 -0500

    react-practice-blog

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navBar/NavBar";
+import News from "../news/News";
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <News />
+        </>
+    )
+}
+
+
+export default Home
---
+++ src/components/news/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import New from "../new/New";
+
+const News = () => {
+  return (
+    <main className="container-fluid d-flex flex-wrap">
+        <New />
+    </main>
+  )
+}
+
+export default News
